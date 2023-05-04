b2a13385cbec6cea77017a11d5ae12c827d0b201
J-Guevara12 <juguevara@unal.edu.co>
2023-05-04 14:35:29 -0500

    Modified classificator program to sample multiple parts of the song and display the distribution of genres

--- classify.py
+++ classify.py
@@ -26,15 +26,31 @@ print(f"Song converted in {round(time.time()-start,3)}\n")
 start = time.time()
 print("Sampling song...\n")
 x,fs = sf.read('./res/music/wav/test.wav')
-xTaken = x[30*fs:(30+5)*fs].copy().T[0]
-xfourier = np.fft.rfft(xTaken)
-n = 1
+#we're going to take at least 10 samples per song, starting at 30s until 165s in 15s lapses
+n = 0
+X = []
+for i in range(30,180,15):
+    # We're only going to take the first channel between 30 and 35 seconds, 60 and 90 and 95
+    if(x.shape[0]>(i+5)*fs): #Checking that we're not sampling away of the song's end
+        n += 1
+        xTaken = x[i*fs:(i+5)*fs].copy().T[0]
+        xfourier = np.fft.rfft(xTaken)
+        
+        X.append(abs(xfourier))
+
 print(f"Took {n} samples of the song in {round(time.time()-start,3)}\n")
 
 loaded_model = pickle.load(open('./res/models/randomForest.pickle','rb'))
-print(loaded_model.predict([abs(xfourier)]))
+predictions = loaded_model.predict(X)
+res = {}
+for pred in predictions:
+    res[pred] = res.setdefault(pred,0) + 1
+
+print("The song has been classified, results are:")
+for genre, i in res.items():
+    print(f"{genre}: {round(i/n*100,3)}%")
 
 os.remove('./res/music/mp3/test.mp3')
 os.remove('./res/music/wav/test.wav')
 
-print(f"the process took: {round(time.time()-START,3)}\n")
+print(f"the process took: {round(time.time()-START,3)}s\n")
